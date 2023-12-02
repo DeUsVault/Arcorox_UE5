149fc0a35371b2ebf276278710f9c25a5950d85c
Sean Patrick <sean.patrick7474@gmail.com>
2023-12-01 19:25:08 -0500

    Enemy idle/running animations

--- Source/Arcorox/Private/Enemy/EnemyAnimInstance.cpp
+++ Source/Arcorox/Private/Enemy/EnemyAnimInstance.cpp
@@ -2,4 +2,33 @@
 
 
 #include "Enemy/EnemyAnimInstance.h"
+#include "Enemy/Enemy.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/KismetMathLibrary.h"
 
+UEnemyAnimInstance::UEnemyAnimInstance() :
+	Speed(0.f)
+{
+
+}
+
+void UEnemyAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	Enemy = Cast<AEnemy>(TryGetPawnOwner());
+	if (Enemy)
+	{
+		EnemyCharacterMovement = Enemy->GetCharacterMovement();
+	}
+}
+
+void UEnemyAnimInstance::NativeUpdateAnimation(float DeltaTime)
+{
+	Super::NativeUpdateAnimation(DeltaTime);
+
+	if (Enemy && EnemyCharacterMovement)
+	{
+		Speed = UKismetMathLibrary::VSizeXY(EnemyCharacterMovement->Velocity);
+	}
+}
--- Source/Arcorox/Public/Enemy/EnemyAnimInstance.h
+++ Source/Arcorox/Public/Enemy/EnemyAnimInstance.h
@@ -6,9 +6,34 @@
 #include "Animation/AnimInstance.h"
 #include "EnemyAnimInstance.generated.h"
 
+class AEnemy;
+class UCharacterMovementComponent;
+
 UCLASS()
 class ARCOROX_API UEnemyAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
 	
+public:
+	UEnemyAnimInstance();
+
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaTime) override;
+
+protected:
+
+
+private:
+	/* Enemy instance reference */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	AEnemy* Enemy;
+
+	/* Enemy character movement component reference */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	UCharacterMovementComponent* EnemyCharacterMovement;
+
+	/* Enemy movement speed */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	float Speed;
+
 };
